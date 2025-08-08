9d591c50511e023d4037c8a3be37406deab6dc8d
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2025-08-08 19:20:45 -0300

    Add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from "@storybook/react-vite";
+import "reactflow/dist/style.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
